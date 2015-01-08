95b43e0302a10444e581ff951052d752d673367f
Akhil Kodali <akhil.kodali@gmail.com>
2015-01-08 01:11:16 -0500

    upgraded akka, scala and 10 parallel actors

--- HelloLocal/src/main/scala/local/Local.scala
+++ HelloLocal/src/main/scala/local/Local.scala
@@ -5,16 +5,36 @@ import akka.actor._
 object Local extends App {
 
   implicit val system = ActorSystem("LocalSystem")
-  val localActor = system.actorOf(Props[LocalActor], name = "LocalActor")  // the local actor
-  localActor ! "START"                                                     // start the action
+  val localActor1 = system.actorOf(Props(new LocalActor(1)),  name = "LocalActor1")  // the local actor
+  val localActor2 = system.actorOf(Props(new LocalActor(2)),  name = "LocalActor2")  // the local actor
+  val localActor3 = system.actorOf(Props(new LocalActor(3)),  name = "LocalActor3")  // the local actor
+  val localActor4 = system.actorOf(Props(new LocalActor(4)),  name = "LocalActor4")  // the local actor
+  val localActor5 = system.actorOf(Props(new LocalActor(5)),  name = "LocalActor5")  // the local actor
+  localActor1 ! 1                                                     // start the action
+  localActor2 ! 1                                                     // start the action
+  localActor3 ! 1                                                     // start the action
+  localActor4 ! 1                                                     // start the action
+  localActor5 ! 1                                                     // start the action
 
+  val localActor6 = system.actorOf(Props(new LocalActor(6)),  name = "LocalActor6")  // the local actor
+  localActor6 ! 1                                                     // start the action
+  val localActor7 = system.actorOf(Props(new LocalActor(7)),  name = "LocalActor7")  // the local actor
+  localActor7 ! 1                                                     // start the action
+  val localActor8 = system.actorOf(Props(new LocalActor(8)),  name = "LocalActor8")  // the local actor
+  localActor8 ! 1                                                     // start the action
+  val localActor9 = system.actorOf(Props(new LocalActor(9)),  name = "LocalActor9")  // the local actor
+  localActor9 ! 1                                                     // start the action
+  val localActor10 = system.actorOf(Props(new LocalActor(10)),  name = "LocalActor10")  // the local actor
+  localActor10 ! 1                                                     // start the action
 }
 
-class LocalActor extends Actor {
+class LocalActor(c: Int) extends Actor {
 
   // create the remote actor
-  val remote = context.actorFor("akka://HelloRemoteSystem@127.0.0.1:5150/user/RemoteActor")
+  val remote = context.actorFor(s"akka.tcp://HelloRemoteSystem@127.0.0.1:5150/user/RemoteActor$c")
   var counter = 0
+
+  var s = System.nanoTime();
 
   def receive = {
     case "START" => 
@@ -25,6 +45,17 @@ class LocalActor extends Actor {
             sender ! "Hello back to you"
             counter += 1
         }
+    case 100000 =>
+      val e = System.nanoTime()
+      println("stop")
+      print((e - s)/1000000000 + " " + new java.util.Date() + "\n")
+
+    case 1 =>
+      s = System.nanoTime();
+      remote ! 1
+
+    case i: Int =>
+      remote ! i
   }
 }
 
--- HelloRemote/src/main/scala/remote/HelloRemote.scala
+++ HelloRemote/src/main/scala/remote/HelloRemote.scala
@@ -4,8 +4,17 @@ import akka.actor._
 
 object HelloRemote extends App  {
   val system = ActorSystem("HelloRemoteSystem")
-  val remoteActor = system.actorOf(Props[RemoteActor], name = "RemoteActor")
+  val remoteActor = system.actorOf(Props[RemoteActor], name = "RemoteActor1")
   remoteActor ! "The RemoteActor is alive"
+  val remoteActor1 = system.actorOf(Props[RemoteActor], name = "RemoteActor2")
+  val remoteActor2 = system.actorOf(Props[RemoteActor], name = "RemoteActor3")
+  val remoteActor3 = system.actorOf(Props[RemoteActor], name = "RemoteActor4")
+  val remoteActor4 = system.actorOf(Props[RemoteActor], name = "RemoteActor5")
+  val remoteActor5 = system.actorOf(Props[RemoteActor], name = "RemoteActor6")
+  val remoteActor6 = system.actorOf(Props[RemoteActor], name = "RemoteActor7")
+  val remoteActor7 = system.actorOf(Props[RemoteActor], name = "RemoteActor8")
+  val remoteActor8 = system.actorOf(Props[RemoteActor], name = "RemoteActor9")
+  val remoteActor9 = system.actorOf(Props[RemoteActor], name = "RemoteActor10")
 }
 
 class RemoteActor extends Actor {
@@ -13,6 +22,8 @@ class RemoteActor extends Actor {
     case msg: String => 
         println(s"RemoteActor received message '$msg'")
         sender ! "Hello from the RemoteActor"
+    case i: Int =>
+       sender ! i+1
   }
 }
 
